66f6fff96e0e6d6a2a6db58409350af4ee959e10
Matthieu Mary <matthieu.mary2@gmail.com>
2021-11-29 10:00:20 +0100

    add contact page with js script for submit

---
+++ main.js
@@ -0,0 +1,7 @@
+const btnSubmit = document.querySelector(".submit");
+
+if (btnSubmit) {
+  btnSubmit.addEventListener("click", () => {
+    alert("Félicitation votre requête à bien été envoyée. Notre équipe traitera votre demande dans les plus brefs délais.");
+  });
+}
