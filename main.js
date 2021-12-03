d1cfca99cbd151a973bf80d8e1ec93af56fa9c67
Matthieu Mary <matthieu.mary2@gmail.com>
2021-12-03 19:23:10 +0100

    Add toggle menu on mobile

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+//NAVBAR HAMBURGER RESPONSIVE (TOGGLE)
+
+const puce = document.querySelector(".container_puce_mobile");
+const menu = document.querySelector(".toggle_menu");
+
+
+let toggleStatus = false;
+
+puce.addEventListener("click", () => {
+
+  if (toggleStatus === false) {
+    menu.style.display = "flex";
+    toggleStatus = true;
+  } else {
+    menu.style.display = "none";
+    toggleStatus = false;
+  }
+
+})
+
+
+
+// PAGE CONTACT
+
 const btnSubmit = document.querySelector(".submit");
 
 if (btnSubmit) {
