b1929ffad92d741359602119bdcbca6eb97645e5
Matthieu Mary <matthieu.mary2@gmail.com>
2021-12-17 19:01:40 +0100

    Replace questions by others and new logic

--- main.js
+++ main.js
@@ -18,11 +18,47 @@ puce.addEventListener("click", () => {
 
 })
 
+
 // FORM CHOICE
 
+const buttonSub = document.querySelector(".sub-button");
+const valueResponseA = document.querySelectorAll(".response-a");
+const valueResponseB = document.querySelectorAll(".response-b");
+const valueResponseC = document.querySelectorAll(".response-c");
 
+const resultTest = () => {
+  let resultResponseA = 0;
+  let resultResponseB = 0;
+  let resultResponseC = 0;
 
+  buttonSub.addEventListener("click", () => {
+        for (let i  = 0; i < valueResponseA.length; i++) {
+          if (valueResponseA[i].checked === true) {
+            resultResponseA++
+          }
+        } 
+        for (let i  = 0; i < valueResponseB.length; i++) {
+          if (valueResponseB[i].checked === true) {
+            resultResponseB++
+          }
+        }   
+        for (let i  = 0; i < valueResponseC.length; i++) {
+          if (valueResponseC[i].checked === true) {
+            resultResponseC++
+          }
+        }
+       
+        if (resultResponseA >= 3) {
+          alert ("La formation Front-End est faite pour vous ! Pour plus d'informations, veuillez consulter notre page 'Formation'")
+        } else if (resultResponseB >= 3) {
+          alert ("La formation Back-End est faite pour vous ! Pour plus d'informations veuillez consulter notre page 'Formation'")
+        } else if (resultResponseC >= 2) {
+          alert ("Vous semblez indécis, vous trouverez des informations approfondies dans notre rubrique 'Formations'..")
+        } 
+})
+}
 
+resultTest();
 
 // PAGE CONTACT
 
