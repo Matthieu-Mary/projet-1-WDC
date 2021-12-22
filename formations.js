0d89087b3855fe8b591aea83917a465fa545ddb9
v-andreeva <veneta-andreeva_student2021@wilder.school>
2021-12-22 18:43:11 +0200

    changement nom png menu burger, adding js on buttons formation page

--- formations.js
+++ formations.js
@@ -1,32 +1,32 @@
 
 //SLIDER FINANCEMENT FORMATIONS
 
-let slideIndex = 1;
-showDivs(slideIndex);
+// let slideIndex = 1;
+// showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
+// function plusDivs(n) {
+//   showDivs(slideIndex += n);
+// }
 
-function showDivs(n) {
-  let i;
-  let x = document.getElementsByClassName("sliderFinanceursItem");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "block";
-}
+// function showDivs(n) {
+//   let i;
+//   let x = document.getElementsByClassName("sliderFinanceursItem");
+//   if (n > x.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = x.length} ;
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+//   x[slideIndex-1].style.display = "block";
+// }
 
-let buttonLeft=document.querySelector('.w3-display-left');
+// let buttonLeft=document.querySelector('.w3-display-left');
 
-buttonLeft.addEventListener('click',plusDivs(){
-    n=-1}
-    );
+// buttonLeft.addEventListener('click',plusDivs(){
+//     n=-1}
+//     );
 
-let buttonRight=document.querySelector('.w3-display-right');
+// let buttonRight=document.querySelector('.w3-display-right');
 
-buttonRight.addEventListener('click',plusDivs(){
-    n=+1}
-    );
+// buttonRight.addEventListener('click',plusDivs(){
+//     n=+1}
+//     );
